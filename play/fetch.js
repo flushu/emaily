960de8016c929f18eb2170c85857c388d33969e5
fffflushu <ben@drsongvision.com>
2018-10-10 22:12:32 -0700

    async/await syntax tried

--- play/fetch.js
+++ play/fetch.js
@@ -2,14 +2,21 @@
 // make an ajax request! Use the 'fetch' function
 // https://rallycoding.herokuapp.com/api/music_albums
 
-function fetchAlbums() {
-  // the async request returns a promise
-  // .then() is called if request is successful with the value returned from the async request
-  fetch("https://rallycoding.herokuapp.com/api/music_albums")
-    .then(res => {
-      return res.json();
-    })
-    .then(json => console.log(json));
+// function fetchAlbums() {
+//   // the async request returns a promise
+//   // .then() is called if request is successful with the value returned from the async request
+//   fetch("https://rallycoding.herokuapp.com/api/music_albums")
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(json => console.log(json));
+// }
+
+async function fetchAlbums() {
+  // put await keyword in front of promise
+  const res = await fetch("https://rallycoding.herokuapp.com/api/music_albums");
+  const json = await res.json();
+  console.log(json);
 }
 
 fetchAlbums();
