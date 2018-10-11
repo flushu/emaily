829c239957b30acc0384fee0e9be884dc5c73e04
fffflushu <ben@drsongvision.com>
2018-10-10 23:02:07 -0700

    install react, redux, and create first component with react

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+// using import because front-end with webpack babel configuration for es15
+import React from "react";
+
+/**
+ * component using jsx
+ */
+const App = () => {
+  return <div>Hello, World!</div>;
+};
+
+export default App;
