25141320810dbc55e7abdc870bcfd6b56df977c7
fffflushu <ben@drsongvision.com>
2018-10-15 23:55:18 -0700

    make use of axios by fetching current_user on /api/current_user to get res and dispatch it

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,12 @@ import { createStore, applyMiddleware } from "redux";
 
 import "materialize-css/dist/css/materialize.min.css"; // need to add file extension unless javascript. also w/ relative path, webpack understands it as we are trying to access npm modules
 // We are going to use Materialize CSS. Has option to use Materialize-UI which is a library for react framework to add Pre-built Materialized Components. Materialize CSS is easy to customize than Materialize-UI because M-UI was made with Javascript and harder to customize. Materialize CSS is made with pure CSS and few Javascript codes; therefore, easier to customize.
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
