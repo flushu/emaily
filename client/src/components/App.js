8f494c102009263f06aec6abe4970f71a0c3b9b4
fffflushu <ben@drsongvision.com>
2018-10-15 23:13:47 -0700

    implementing materialize-css by adding a nav to the app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,6 @@ import { BrowserRouter, Route } from "react-router-dom";
 // Route: React component used to setup rules between certain routes inside applications
 import Header from "./Header";
 
-// We are going to use Materialize CSS. Has option to use Materialize-UI which is a library for react framework to add Pre-built Materialized Components. Materialize CSS is easy to customize than Materialize-UI because M-UI was made with Javascript and harder to customize. Materialize CSS is made with pure CSS and few Javascript codes; therefore, easier to customize.
-
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -15,8 +13,9 @@ const Landing = () => <h2>Landing</h2>;
  * component using jsx
  */
 const App = () => {
+  // materialize css requires us to add class container to root of components
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
           <Header />
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
+import "materialize-css/dist/css/materialize.min.css"; // need to add file extension unless javascript. also w/ relative path, webpack understands it as we are trying to access npm modules
+// We are going to use Materialize CSS. Has option to use Materialize-UI which is a library for react framework to add Pre-built Materialized Components. Materialize CSS is easy to customize than Materialize-UI because M-UI was made with Javascript and harder to customize. Materialize CSS is made with pure CSS and few Javascript codes; therefore, easier to customize.
+
 import App from "./components/App";
 import reducers from "./reducers";
 
