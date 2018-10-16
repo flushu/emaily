b2f5dc386dd75f0c4206587a548b5dec0caaf2a9
fffflushu <ben@drsongvision.com>
2018-10-15 22:36:08 -0700

    set up react-router with bot components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,30 @@
 // using import because front-end with webpack babel configuration for es15
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+// BrowserRouter: tells React how to behave
+// Route: React component used to setup rules between certain routes inside applications
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>Survey New</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 /**
  * component using jsx
  */
 const App = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} exact={true} />
+          <Route path="/survey" component={Dashboard} />
+          <Route />
+          <Route />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
