932b0652d80204a61c35af53447b52690aca4b09
fffflushu <ben@drsongvision.com>
2018-10-16 00:20:08 -0700

    refactor redux action with async and await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,18 @@
 import axios from "axios"; // library to call AJAX
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  return dispatch => {
-    axios
-      .get("/api/current_user") // call get request to back-end
-      .then(res =>
-        dispatch({
-          type: FETCH_USER,
-          payload: res
-        })
-      );
-  };
+export const fetchUser = () => async dispatch => {
+  // axios
+  //   .get("/api/current_user") // call get request to back-end
+  //   .then(res =>
+  //     dispatch({
+  //       type: FETCH_USER,
+  //       payload: res
+  //     })
+  //   );
+  const res = await axios.get("/api/current_user");
+  dispatch({
+    type: FETCH_USER,
+    payload: res
+  });
 };
