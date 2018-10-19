59809e1d2a6cbe78c3d7aaaf2aa6d381a5e46eef
fffflushu <ben@drsongvision.com>
2018-10-18 20:08:02 -0700

    add credits property to the User model. I also add a logic for non-authenticated user to be blocked and gets error with status code 401. Wrote arithmatic to the user model to add credits when posted

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,12 +3,18 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post("/api/stripe", async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: "You must log in!" }); // returns error with status code 401
+    }
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 credits",
       source: req.body.id
     });
-    console.log(charge);
+    req.user.credits += 5;
+    const updatedUser = await req.user.save();
+
+    res.send(updatedUser);
   });
 };
